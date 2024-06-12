e7a9f6e34a1eddc99f98da04f4b02b0f22208280
inesbl113 <inesbl113@gmail.com>
2024-06-12 17:06:52 +0200

    New

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BibliothequeCSWebAPI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
